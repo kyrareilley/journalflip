1bf31a9d2c967f901379f7ebb77f48e167cc9e74
kyrareilley <kr3162@nyu.edu>
2025-07-01 14:16:34 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -128,184 +128,120 @@ function reset_pages() {
   mid8_2.style.display = "none";
 }
 
+function hide_sticky(){
+  sticky.style.display = "none";
+}
+
 function setup() {
   createCanvas(620, 515);
+
 }
 
 function draw() {
-  background(242, 240, 237);
+//  background(242, 240, 237);
+background(255, 255, 255);
   noFill();
   stroke(0);
-  bar = rect(50, 500, 520, 10, 10);
+  bar = rect(35, 500, 550, 10, 10);
   fill(0);
-  bar = rect(50 + position / 2, 500, 50, 10, 10);
+  bar = rect(35 + position / 2, 500, 50, 10, 10);
 
   // COVER
   if (position < 100) {
     reset_pages();
     cover.style.display = "block";
-    page1.style.display = "none";
-    //bar = rect(50, 500, 50, 10, 10);
     // PAGE 1
   } else if (position >= 100 && position < 150) {
     reset_pages();
-    cover.style.display = "none";
+    hide_sticky();
     page1.style.display = "block";
-    mid1_1.style.display = "none";
-    sticky.style.display = "none";
-    // bar = rect(100, 500, 50, 10, 10);
     // mid 1 & mid 2 from 150 to 200
     // bar will move accordingly so split into 17, 34, 50 / 67, 84, 100
   } else if (position >= 150 && position < 175) {
     reset_pages();
-    page1.style.display = "none";
+    hide_sticky();
     mid1_1.style.display = "block";
-    mid1_2.style.display = "none";
-    // bar = rect(117, 500, 50, 10, 10);
   } else if (position >= 175 && position < 200) {
     reset_pages();
-    mid1_1.style.display = "none";
+    hide_sticky();
     mid1_2.style.display = "block";
-    page2.style.display = "none";
-    // bar = rect(134, 500, 50, 10, 10);
     // PAGE 2
   } else if (position >= 200 && position < 250) {
     reset_pages();
-    mid1_2.style.display = "none";
+    hide_sticky();
     page2.style.display = "block";
-    mid2_1.style.display = "none";
-    //bar = rect(150, 500, 50, 10, 10);
   } else if (position >= 250 && position < 275) {
     reset_pages();
-    page2.style.display = "none";
+    hide_sticky();
     mid2_1.style.display = "block";
-    mid2_2.style.display = "none";
-    //  bar = rect(167, 500, 50, 10, 10);
   } else if (position >= 275 && position < 300) {
     reset_pages();
-    mid2_1.style.display = "none";
+    hide_sticky();
     mid2_2.style.display = "block";
-    page3.style.display = "none";
-    //  bar = rect(184, 500, 50, 10, 10);
     // PAGE 3
   } else if (position >= 300 && position < 350) {
     reset_pages();
-    mid2_2.style.display = "none";
     page3.style.display = "block";
-    mid3_1.style.display = "none";
-    //   bar = rect(200, 500, 50, 10, 10);
   } else if (position >= 350 && position < 375) {
     reset_pages();
-    page3.style.display = "none";
     mid3_1.style.display = "block";
-    mid3_2.style.display = "none";
-    // bar = rect(217, 500, 50, 10, 10);
   } else if (position >= 375 && position < 400) {
     reset_pages();
-    mid3_1.style.display = "none";
     mid3_2.style.display = "block";
-    page4.style.display = "none";
     bar = rect(234, 500, 50, 10, 10);
     // PAGE 4
   } else if (position >= 400 && position < 450) {
     reset_pages();
-    mid3_2.style.display = "none";
     page4.style.display = "block";
-    mid4_1.style.display = "none";
-    //bar = rect(250, 500, 50, 10, 10);
   } else if (position >= 450 && position < 475) {
     reset_pages();
-    page4.style.display = "none";
     mid4_1.style.display = "block";
-    mid4_2.style.display = "none";
-    // bar = rect(267, 500, 50, 10, 10);
   } else if (position >= 475 && position < 500) {
     reset_pages();
-    mid4_1.style.display = "none";
     mid4_2.style.display = "block";
-    page5.style.display = "none";
-    // bar = rect(284, 500, 50, 10, 10);
     // PAGE 5
   } else if (position >= 500 && position < 550) {
     reset_pages();
-    mid4_2.style.display = "none";
     page5.style.display = "block";
-    mid5_1.style.display = "none";
-    // bar = rect(300, 500, 50, 10, 10);
   } else if (position >= 550 && position < 575) {
     reset_pages();
-    page5.style.display = "none";
     mid5_1.style.display = "block";
-    mid5_2.style.display = "none";
-    // bar = rect(317, 500, 50, 10, 10);
   } else if (position >= 575 && position < 600) {
     reset_pages();
-    mid5_1.style.display = "none";
     mid5_2.style.display = "block";
-    page6.style.display = "none";
-    //  bar = rect(334, 500, 50, 10, 10);
     // PAGE 6
   } else if (position >= 600 && position < 650) {
     reset_pages();
-    mid5_2.style.display = "none";
     page6.style.display = "block";
-    mid6_1.style.display = "none";
-    //bar = rect(350, 500, 50, 10, 10);
   } else if (position >= 650 && position < 675) {
     reset_pages();
-    page6.style.display = "none";
     mid6_1.style.display = "block";
-    mid6_2.style.display = "none";
-    //bar = rect(367, 500, 50, 10, 10);
   } else if (position >= 675 && position < 700) {
     reset_pages();
-    mid6_1.style.display = "none";
     mid6_2.style.display = "block";
-    page7.style.display = "none";
-    // bar = rect(384, 500, 50, 10, 10);
     // PAGE 7
   } else if (position >= 700 && position < 750) {
     reset_pages();
-    mid6_2.style.display = "none";
     page7.style.display = "block";
-    mid7_1.style.display = "none";
-    //bar = rect(400, 500, 50, 10, 10);
   } else if (position >= 750 && position < 775) {
     reset_pages();
-    page7.style.display = "none";
     mid7_1.style.display = "block";
-    mid7_2.style.display = "none";
-    // bar = rect(417, 500, 50, 10, 10);
   } else if (position >= 775 && position < 800) {
     reset_pages();
-    mid7_1.style.display = "none";
     mid7_2.style.display = "block";
-    page8.style.display = "none";
-    // bar = rect(434, 500, 50, 10, 10);
     // PAGE 8
   } else if (position >= 800 && position < 850) {
     reset_pages();
-    mid7_2.style.display = "none";
     page8.style.display = "block";
-    mid8_1.style.display = "none";
-    //  bar = rect(450, 500, 50, 10, 10);
   } else if (position >= 850 && position < 875) {
     reset_pages();
-    page8.style.display = "none";
     mid8_1.style.display = "block";
-    mid8_2.style.display = "none";
-    //bar = rect(477, 500, 50, 10, 10);
   } else if (position >= 875 && position < 900) {
     reset_pages();
-    mid8_1.style.display = "none";
     mid8_2.style.display = "block";
-    page9.style.display = "none";
-    // bar = rect(500, 500, 50, 10, 10);
     // PAGE 9
   } else if (position >= 900) {
     reset_pages();
-    mid8_2.style.display = "none";
     page9.style.display = "block";
-    // bar = rect(520, 500, 50, 10, 10);
   }
 }
